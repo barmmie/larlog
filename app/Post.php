26c09c99e924d3fc5f0abf7531de838595302bc8
INTERSWITCHNG\opeyemi.aina <opeyemi.aina@interswitchgroup.com>
2017-10-17 15:30:07 +0100

    init

--- app/Post.php
+++ app/Post.php
@@ -5,8 +5,21 @@ namespace App;
 class Post extends Model {
 
 
-    public static function saveNewPost() {
-        
+    public static function saveNewPost($user, $request) {
+        $post = new static;
+        $post->user_id = $user->id;
+        $post->slug = static::slugify($request['title'], $user->id);
+        $post->title = $request['title'];
+        $post->description = $request['description'];
+
+        $post->save();
+    }
+
+
+    public static function slugify($title) {
+        $slug = $user->id.'-'.str_slug('title', '-'). '-' . time();
+
+        return $slug;
     }
  
 }
